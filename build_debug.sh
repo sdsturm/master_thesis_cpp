e5f54a03808b7d62250901da590c01e0f93ac472
Simon Sturm <sturmsimon@t-online.de>
2021-09-03 14:06:54 +0200

    Deleted shell scripts.

--- build_debug.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-mkdir debug
-cd debug
-cmake -DCMAKE_BUILD_TYPE=Debug -S .. -B .
-make
--- build_release.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-mkdir release
-cd release
-cmake -DCMAKE_BUILD_TYPE=Debug -S .. -B .
-make
