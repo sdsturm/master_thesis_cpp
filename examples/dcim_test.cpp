99e2b5c927e262c1208666117e0269ba2639971a
Simon Sturm <sturmsimon@t-online.de>
2021-09-03 17:25:04 +0200

    Refined DCIM example.

--- examples/dcim_test.cpp
+++ examples/dcim_test.cpp
@@ -2,11 +2,19 @@
 
 #include <gsl/gsl_const_mksa.h>
 
+#include <random>
+
 using namespace mthesis;
 
-VectorR3 rand_point(real factor)
+VectorR3 rand_point(real dist)
 {
-    return (2 * arma::randu(3) - 1) * factor;
+    std::random_device dev;
+    std::mt19937 gen(dev());
+    std::uniform_real_distribution<double> dist_pos(0.0, dist);
+
+    VectorR3 r = {dist_pos(gen), dist_pos(gen), dist_pos(gen)};
+
+    return r;
 }
 
 int main()
@@ -19,11 +27,8 @@ int main()
     auto mode = EmMode::TM;
 
      auto r = rand_point(5 * fd.lambda_0);
-     auto r_ = rand_point(5 * fd.lambda_0);
-//    VectorR3 r = {5, 0, 1};
-//    VectorR3 r_ = {0, 0, 1};
-    r *= fd.lambda_0;
-    r_ *= fd.lambda_0;
+     VectorR3 r_ = {0, 0, 0};
+//     auto r_ = rand_point(5 * fd.lambda_0);
 
     std::cout << "r = \n" << r << "\n";
     std::cout << "r_ = \n" << r_ << "\n";
@@ -37,12 +42,11 @@ int main()
     auto val_ref = si::eval_along_sip(gf, nu, coords.rho);
 
     auto ce_levels = dcim::threelevelv2::three_level_v2(gf);
-
     auto val_dcim = dcim::utils::get_spectral_gf(ce_levels, coords.rho, fd.k_0);
 
     std::cout << "val_ref:        " << val_ref << "\n";
     std::cout << "val_dcim:       " << val_dcim << "\n";
-    std::cout << "Relative error: " << calc_rel_err_db(val_dcim, val_ref) << " dB\n";
+    std::cout << "Error: " << calc_rel_err_db(val_dcim, val_ref) << " dB\n";
 
     return 0;
 }
