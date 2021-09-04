77100c947ee527b0c3efdfbc512a33e0b2c1116a
Simon Sturm <sturmsimon@t-online.de>
2021-09-04 12:36:16 +0200

    Sommerfeld ID example.

--- production/numerical_integration_sommerfeld_identity.cpp
+++ production/numerical_integration_sommerfeld_identity.cpp
@@ -1,6 +1,7 @@
 #include <mthesis.hpp>
 
 #include <armadillo>
+#include <boost/timer/timer.hpp>
 
 #include <filesystem>
 #include <cstdio>
@@ -21,10 +22,8 @@ int main()
     arma::vec x_vals = arma::logspace(-1, 3, 60) * fd.lambda_0;
     arma::vec z_vals = arma::logspace(-1, 3, 60) * fd.lambda_0;
 
-    real max_rel_err_db = -INFINITY;
-
     auto data_file_path = std::filesystem::path(MTHESIS_RESULTS_DIR);
-    data_file_path /= "num_int_sommerfeld_id_err.txt";
+    data_file_path /= "numerical_integration_sommerfeld_identity.dat";
     FILE *data_file;
     data_file = fopen(data_file_path.c_str(), "w");
     if (!data_file)
@@ -32,33 +31,42 @@ int main()
         std::cerr << "Unable to open TEX data output file.\n";
         return -1;
     }
-    fprintf(data_file, "x_by_lambda_0  z_by_lambda_0  rel_err_db \n");
-
+    fprintf(data_file, "x_by_lambda_0 z_by_lambda_0 rel_err_db time\n");
+    size_t n = 1;
+    size_t N = x_vals.size() * z_vals.size();
+    boost::timer::auto_cpu_timer timer;
     for (auto &x : x_vals)
     {
         for (auto &z : z_vals)
         {
             VectorR3 r = {x, 0, z};
-
-            auto num = sgf::lm_generic_spatial(lm, r, r_, mode, direct_term);
             auto ref = sgf::free_space(vacuum, r, r_);
 
-            auto rel_err_db = calc_rel_err_db(num, ref);
-            printf("x = %.2e   z = %.2e   err = %.2f dB\n", x, z, rel_err_db);
+            // Multiple runs for timer.
+            int N_runs = 50;
+            timer.start();
+            for (int i = 0; i < N_runs - 1; i++)
+                sgf::lm_generic_spatial(lm, r, r_, mode, direct_term);
+            // One final time for the result.
+            auto num = sgf::lm_generic_spatial(lm, r, r_, mode, direct_term);
+            timer.stop();
 
-            fprintf(data_file, "%.12f  %.12f  %.12f  \n",
+            auto rel_err_db = calc_rel_err_db(num, ref);
+            auto t_str = timer.format(9, "%u");
+
+            fprintf(data_file, "%.12f %.12f %.12f %s\n",
                     x / fd.lambda_0,
                     z / fd.lambda_0,
-                    rel_err_db);
+                    rel_err_db,
+                    t_str.c_str());
 
-            if (rel_err_db > max_rel_err_db)
-                max_rel_err_db = rel_err_db;
+            printf("Processing point %4ld of %4ld\n", n, N);
+            n++;
         }
         fprintf(data_file, "\n");
     }
+    printf("Done.\n");
     fclose(data_file);
-
-    printf("\n\nMaximum error: %.2f dB\n", max_rel_err_db);
 
     return 0;
 }
