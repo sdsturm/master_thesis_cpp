961db605132cffed81a64a5192bd0793c9098715
Simon Sturm <sturmsimon@t-online.de>
2021-09-09 11:38:46 +0200

    Skeleton for DCIM tests

---
+++ lib/test/test_dcim.cpp
@@ -0,0 +1,12 @@
+#define BOOST_TEST_DYN_LINK
+#include <boost/test/unit_test.hpp>
+
+#include <boost/test/data/test_case.hpp>
+#include <boost/test/data/monomorphic.hpp>
+
+#include <mthesis/dcim/gpof.hpp>
+#include <mthesis/dcim/synth_exponentials.hpp>
+
+BOOST_AUTO_TEST_SUITE(DCIM)
+
+BOOST_AUTO_TEST_SUITE_END()
