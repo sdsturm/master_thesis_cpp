f094eb83098355f7afbcec10ceba2e4f98cbfc2b
Simon Sturm <sturmsimon@t-online.de>
2021-09-19 16:18:42 +0200

    Done with standard FMM production code

--- production/thesis_fmm_standard_accuracy.cpp
+++ production/thesis_fmm_standard_accuracy.cpp
@@ -1,26 +1,14 @@
 #include <mthesis.hpp>
 
 #include <armadillo>
-#include <gsl/gsl_const_mksa.h>
-#include <boost/timer/timer.hpp>
 
-#include <random>
 #include <cstdio>
 
 using namespace mthesis;
 
-fmm::multiindex rand_mi()
-{
-    std::random_device rd;
-    std::mt19937 gen(rd());
-    std::uniform_int_distribution<int> dist(0, 100);
-
-    return fmm::multiindex {dist(gen), dist(gen), dist(gen)};
-}
-
 int main()
 {
-    FrequencyDomain fd(GSL_CONST_MKSA_SPEED_OF_LIGHT);
+    FrequencyDomain fd(1e9);
     real w = 1.0 * fd.lambda_0;
     fmm::Params params(fd, w);
 
@@ -28,8 +16,8 @@ int main()
     auto src_pts = fmm::rand_pts_in_group(params, {0, 0, 20}, 500);
 
     // Observation points: in xy-plane.
-    arma::vec x_vals = arma::linspace(-100, 100, 50) * w;
-    arma::vec y_vals = arma::linspace(-100, 100, 50) * w;
+    arma::vec x_vals = arma::logspace(1, 4, 60) * fd.lambda_0;
+    arma::vec y_vals = arma::logspace(1, 4, 60) * fd.lambda_0;
     std::vector<VectorR3> obs_pts;
     obs_pts.reserve(x_vals.n_elem * y_vals.n_elem);
     for (const auto &x : x_vals) {
@@ -41,38 +29,34 @@ int main()
     // Excitation vector.
     std::vector<cmplx> I(src_pts.size(), 1.0);
 
-    boost::timer::auto_cpu_timer timer;
-
     // FMM solution.
-    printf("FMM setup: ");
-    timer.start();
     fmm::freespace::FMM my_fmm(params, src_pts, obs_pts);
-    printf("%s seconds\n", timer.format(9, "%u").c_str());
-
-    printf("FMM solution: ");
-    timer.start();
     auto V_fmm = my_fmm.calc_product(I);
-    printf("%s seconds\n", timer.format(9, "%u").c_str());
 
     // MoM solution.
-    printf("MoM setup: ");
-    timer.start();
     mom::MoM my_mom(fd, src_pts, obs_pts);
-    printf("%s\n", timer.format(9, "%u").c_str());
-
-    printf("MoM solution: ");
-    timer.start();
     auto V_mom = my_mom.calc_product(I);
-    printf("%s\n", timer.format(9, "%u").c_str());
 
-    // Compute error.
-    std::vector<real> rel_err_db(obs_pts.size());
-    for (size_t n = 0; n < obs_pts.size(); n++) {
-        rel_err_db[n] = calc_rel_err_db(V_fmm[n], V_mom[n]);
+    // Print informations into header comments.
+    printf("# src groups:          %lu\n", my_fmm.src_groups.size());
+    printf("# obs groups:          %lu\n", my_fmm.obs_groups.size());
+    printf("# TOPs:                %lu\n", my_fmm.top_all.size());
+    printf("# plane waves:         %lu\n", my_fmm.es.k_hat.size());
+    printf("# multipole order: L = %u\n", my_fmm.L);
+    // Print error.
+    printf("x_by_lambda_0 y_by_lambda_0 rel_err_db\n");
+    unsigned n = 0;
+    for (const auto &x : x_vals) {
+        for (const auto &y : y_vals) {
+            real rel_err_db = calc_rel_err_db(V_fmm[n], V_mom[n]);
+            n++;
+            printf("%.6e %.6e %.6f\n",
+                   x / fd.lambda_0,
+                   y / fd.lambda_0,
+                   rel_err_db);
+        }
+        printf("\n");
     }
-
-    printf("Maximum error: %.2f dB\n",
-           *std::max_element(rel_err_db.begin(), rel_err_db.end()));
 
     return 0;
 }
